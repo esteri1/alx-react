b8c88475438fa9baa35ee61f2fe277005abef744
esteri1 <eadeyemi24@gmail.com>
2023-05-25 12:05:16 +0000

    App.test.js

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -1,49 +1,52 @@
-import React from "react";
-import App from "./App";
-import Login from "../Login/Login";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Notifications from "../Notifications/Notifications";
-import CourseList from "../CourseList/CourseList";
-import { shallow } from "enzyme";
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import './App.css';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
 
-describe("App tests", () => {
-  it("renders without crashing", () => {
-    const component = shallow(<App />);
+class App extends React.Component {
+	listCourses = [
+		{ id: 1, name: 'ES6', credit: 60 },
+		{ id: 2, name: 'Webpack', credit: 20 },
+		{ id: 3, name: 'React', credit: 40 },
+	];
 
-    expect(component).toBeDefined();
-  });
-  it("should render Notifications component", () => {
-    const component = shallow(<App />);
+	listNotifications = [
+		{ id: 1, type: 'default', value: 'New course available' },
+		{ id: 2, type: 'urgent', value: 'New resume available' },
+		{ id: 3, type: 'urgent', html: getLatestNotification() },
+	];
 
-    expect(component.containsMatchingElement(<Notifications />)).toEqual(false);
-  });
-  it("should render Header component", () => {
-    const component = shallow(<App />);
+	render() {
+		return (
+			<React.Fragment>
+				<div className='App'>
+					<div className='heading-section'>
+						<Notifications listNotifications={this.listNotifications} />
+						<Header />
+					</div>
+					{this.props.isLoggedIn ? (
+						<CourseList listCourses={this.listCourses} />
+					) : (
+						<Login />
+					)}
+					<Footer />
+				</div>
+			</React.Fragment>
+		);
+	}
+}
 
-    expect(component.contains(<Header />)).toBe(true);
-  });
-  it("should render Login Component", () => {
-    const component = shallow(<App />);
+App.defaultProps = {
+	isLoggedIn: false,
+};
 
-    expect(component.contains(<Login />)).toBe(true);
-  });
-  it("should render Footer component", () => {
-    const component = shallow(<App />);
+App.propTypes = {
+	isLoggedIn: PropTypes.bool,
+};
 
-    expect(component.contains(<Footer />)).toBe(true);
-  });
-  it("does not render courselist if logged out", () => {
-    const component = shallow(<App />);
-
-    component.setProps({ isLogedIn: false });
-
-    expect(component.contains(<CourseList />)).toBe(false);
-  });
-  it("renders courselist if logged in", () => {
-    const component = shallow(<App isLoggedIn={true} />);
-
-    expect(component.containsMatchingElement(<CourseList />)).toEqual(false);
-    expect(component.contains(<Login />)).toBe(false);
-  });
-});
+export default App;
